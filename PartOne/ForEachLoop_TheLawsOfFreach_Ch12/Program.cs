e495985e8937807bd19b31c89937c3e162754ed5
Aaron Garcia <aaron.m.garcia34@outlook.com>
2022-10-30 21:44:20 -0600

    Foreach and array exercise. find minimum and average values of an array using foreach.

---
+++ PartOne/ForEachLoop_TheLawsOfFreach_Ch12/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] firstArray = new int[5] { 5, 7, 1, 45, 4 };
+int minNumber = firstArray[0];
+double averageArray = 0;
+
+foreach (int i in firstArray)   // Get min value
+{
+    if (i < minNumber)
+    {
+        minNumber = i;
+    }
+}
+
+foreach (int i in firstArray)   // Get Average
+{
+    averageArray += i;
+}
+averageArray /= firstArray.Length;
+
+Console.WriteLine(" minNumber " + minNumber + " " + "average array " + averageArray);
