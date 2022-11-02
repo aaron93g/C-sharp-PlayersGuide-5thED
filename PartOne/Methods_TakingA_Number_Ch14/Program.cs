a29b05140560435637e297cd0bf92f0ce49f4c6c
Aaron Garcia <aaron.m.garcia34@outlook.com>
2022-11-01 18:26:00 -0600

    Add project files.

---
+++ PartOne/Methods_TakingA_Number_Ch14/Program.cs
@@ -0,0 +1,25 @@
+﻿///<summary>
+///intro to methods. 
+/// </summary>
+/// 
+
+int AskForNumber(string text)
+{
+    Console.WriteLine(text);
+    return Int32.Parse(Console.ReadLine());
+
+}
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    while (true)
+    {
+        int triedNumber = AskForNumber(text);
+        if (min <= triedNumber && max >= triedNumber)
+            return triedNumber;
+    }
+}
+
+//int result = AskForNumber("what is the airspeed velocity of an unladen swallow?");
+
+int result = AskForNumberInRange(" Enter a number within 0 and 100 ", 0, 100);
