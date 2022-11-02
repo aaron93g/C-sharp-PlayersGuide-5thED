759ad48dd2c5d3a36724a519dea568aafeac0b55
Aaron Garcia <aaron.m.garcia34@outlook.com>
2022-11-01 21:52:52 -0600

    Add project files.

---
+++ PartOne/LVL_13-learn_recursions_Countdown/Program.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// make method that take int and subtracts by 1 and stops at 1.
+/// have method call self to keep subtracting
+/// </summary>
+/// 
+
+int countDownRecursion(int start)
+{
+    if (start == 1)
+    {
+        Console.WriteLine(start);
+        return start;
+    }
+    Console.WriteLine(start);
+    return countDownRecursion(start - 1);
+}
+
+countDownRecursion(10);
