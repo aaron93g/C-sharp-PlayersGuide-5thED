47f24ba8b64c395202c484544560d7fcda5c41f9
Aaron Garcia <aaron.m.garcia34@outlook.com>
2022-11-11 11:47:11 -0700

    .

--- Part-Two_OOP/Lvl-17_Tuples/Simulas_Soup/Program.cs
+++ Part-Two_OOP/Lvl-17_Tuples/Simulas_Soup/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-
+/*
 (Receipe, Ingredient, Seasoning) dish;
 string plate;
 string vegetable;
@@ -22,9 +22,67 @@ if(plate == "Stew" && vegetable == "Carrot" && taste == "Salty")
 {
     Console.WriteLine($"{Seasoning.Salty} {Ingredient.Carrot} {Receipe.Stew}!");
 }
+*/
+
+//int name = value
+//create a tuple variable with types of the enums and assign values to the variable
+(Receipe type, Ingredient vegitable, Seasoning season) dish = assignDish();
+Console.WriteLine($"{dish.season} {dish.vegitable} {dish.type}");
+
+
+// create a methode that returns values of the types specified!
+(Receipe, Ingredient, Seasoning) assignDish()
+{
+    // make methods to return values to variables
+    Receipe type = assignReceipe();
+    Ingredient vegitable = assignIngredient();
+    Seasoning season = assignSeasoning();
+
+    // return the variables
+    return (type, vegitable, season);
+}
+
+
+Receipe assignReceipe()
+{
+    Console.WriteLine("choose dish type (soup, stew, gumbo): ");
+    string input = Console.ReadLine();
+    return input switch
+    {
+        "soup" => Receipe.Soup,
+        "stew" => Receipe.Stew,
+        "gumbo" => Receipe.Gumbo
+    };
+}
+
+Ingredient assignIngredient()
+{
+    Console.WriteLine(" choose main ingredient for dish (mushroom, chicken, carrot, potatoe): ");
+    string input = Console.ReadLine();
+    return input switch
+    {
+        "mushroom" => Ingredient.Mushroom,
+        "chicken" => Ingredient.Chicken,
+        "carrot" => Ingredient.Carrot,
+        "potatoe" => Ingredient.Potatoe,
+    };
+}
+
+Seasoning assignSeasoning()
+{
+    Console.WriteLine("Pick season for dish (salty, spicy, sweet): ");
+    string input = Console.ReadLine();
+    return input switch
+    {
+        "salty" => Seasoning.Salty,
+        "spicy" => Seasoning.Spicy,
+        "sweet" => Seasoning.Sweet,
+    };
+}
+
 
 
 
 enum Receipe { Soup, Stew, Gumbo }
-enum Ingredient { Mushroom, Checken, Carrot, Potatoe }
+enum Ingredient { Mushroom, Chicken, Carrot, Potatoe }
 enum Seasoning { Spicy, Salty, Sweet }
